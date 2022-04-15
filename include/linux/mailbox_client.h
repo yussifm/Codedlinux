39a889ee54a670967610f7d8578955c50da446e4
Hector Martin <marcan@marcan.st>
2022-04-15 23:57:51 +0900

    mailbox: Rename peek_data to poll_data and fix documentation
    peek_data was poorly specified and the documentation implied it should
    just check for whether there are messages available to be received.
    The intent is for this function to be usable in atomic context. Just
    checking for available data is not useful; the subsystem needs to
    provide some way to poll for (and actually receive) data in atomic
    context for this to make sense.
    
    As one might expect, all existing drivers that implemented the "peek"
    semantics had zero actual users, since that interpretation of the
    feature doesn't lend itself to a useful purpose. There is only one
    driver implementing peek_data as a proper poll: flexrm. That also
    happens to be the only one with a user.
    
    So, rename peek_data to poll_data and fix the documentation to actually
    describe it properly as a synchronous poll for data. Previous patches
    already removed the useless implementations, so this only has to fix the
    bcm driver and client besides the subsystem.
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- include/linux/mailbox_client.h
+++ include/linux/mailbox_client.h
@@ -43,7 +43,7 @@ struct mbox_chan *mbox_request_channel(struct mbox_client *cl, int index);
 int mbox_send_message(struct mbox_chan *chan, void *mssg);
 int mbox_flush(struct mbox_chan *chan, unsigned long timeout);
 void mbox_client_txdone(struct mbox_chan *chan, int r); /* atomic */
-bool mbox_client_peek_data(struct mbox_chan *chan); /* atomic */
+bool mbox_client_poll_data(struct mbox_chan *chan); /* atomic */
 void mbox_free_channel(struct mbox_chan *chan); /* may sleep */
 
 #endif /* __MAILBOX_CLIENT_H */
